2881147d4c720936f3dbd19bfefaf0ecccbe79fd
Emmanuel Oropeza <emmanueljoseoropezatorres@gmail.com>
2026-05-02 09:35:17 -0400

    subir ejemplo

---
+++ ejemplo01.py
@@ -0,0 +1,17 @@
+# Ejemplo de Python para Formación complementaria
+x = 5
+y = 10
+
+print ("El valor de 5 * 10 es: ", x * y)
+print ("Realizado") 
+
+nombre = input ("Coloca tu primer nombre ")
+apellido = input ("Coloca tu primer aapellido ")
+
+print ("El nombre completo es : " + nombre + " " + apellido)
+
+edad = int(input("ingrese su edad: "))
+total = edad + 1500
+print (total)
+
+ 
